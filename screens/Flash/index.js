6011bef6bec07d8eb11ccfa7167290378de0cab7
Parimal Raj <parimal@alphaskynet.com>
2021-11-22 21:06:05 +0530

    Flash screen base work init

---
+++ screens/Flash/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View,Text } from "react-native";
+import style from "./style";
+export default function Flash(){
+    return(
+        <View style={style.container}>
+            <Text>Flash screen</Text>
+        </View>
+    )
+}
